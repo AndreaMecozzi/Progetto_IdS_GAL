bc4a9fffd9b094dc347bd5ecc0f6234887fdfc68
AndreaMecozzi <andrea.mecozzi@studenti.unicam.it>
2026-03-02 19:57:21 +0100

    Creazione progetto java

---
+++ Progetto_Hackhub_GAL/src/main/java/unicam/ids2526/gal/progetto_hackhub_gal/ProgettoHackhubGalApplication.java
@@ -0,0 +1,13 @@
+package unicam.ids2526.gal.progetto_hackhub_gal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoHackhubGalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoHackhubGalApplication.class, args);
+    }
+
+}
---
+++ Progetto_Hackhub_GAL/src/test/java/unicam/ids2526/gal/progetto_hackhub_gal/ProgettoHackhubGalApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.ids2526.gal.progetto_hackhub_gal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoHackhubGalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
