334f1c1fa7d9369de0b08ea9703cdf896e496e42
lorenzo01pompei <lorenzo01.pompei@studenti.unicam.it>
2026-03-20 14:18:21 +0100

    modifica vpp sequence diagram
    revisione dei sequence diagram

--- Progetto_Hackhub_GAL/src/main/java/unicam/ids2526/gal/progetto_hackhub_gal/infrastructure/TeamRepository.java
+++ Progetto_Hackhub_GAL/src/main/java/unicam/ids2526/gal/progetto_hackhub_gal/infrastructure/TeamRepository.java
@@ -3,10 +3,13 @@ package unicam.ids2526.gal.progetto_hackhub_gal.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import unicam.ids2526.gal.progetto_hackhub_gal.core.hackathon.Hackathon;
 import unicam.ids2526.gal.progetto_hackhub_gal.core.team.Team;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team,Long> {
     Optional<Team> findByUtenti_Username(String username);
     Optional<Hackathon> findHackathonByTeamId(Long teamId);
     Optional<Team> findByNome(String nome);
+
 }
